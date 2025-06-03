6824b635b2ecc2d94a5f1e3893bc3dfcd21565f5
phdwight <phdwight@live.com>
2025-06-03 09:57:11 +0000

    chore: add gh-pages as a dev dependency

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/frontend/', // Set to your repo name if deploying to GitHub Pages
   plugins: [
     react(),
     VitePWA({
